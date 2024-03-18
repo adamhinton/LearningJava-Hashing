cfcf4204aad489485fb0e785193542e81b678f3a
adamd <adam.d.hinton@gmail.com>
2024-03-18 12:32:01 -0400

    override hashcode and equals

--- src/dev/lpa/PlayingCard.java
+++ src/dev/lpa/PlayingCard.java
@@ -6,14 +6,24 @@ public class PlayingCard {
     private String face;
     private int internalHash;
 
-    public PlayingCard(String suit, String face, int internalHash) {
+    public PlayingCard(String suit, String face) {
         this.suit = suit;
         this.face = face;
-        this.internalHash = internalHash;
+        this.internalHash = 1;
     }
 
     @Override
     public String toString() {
         return face + " of " + suit;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
